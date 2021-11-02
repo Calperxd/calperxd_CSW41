f2c4f085df06d4ae40ae3c10234db1e8d10b608c
Gabriel Calper Seabra <gabrielseabra@alunos.utfpr.edu.br>
2021-11-02 16:17:16 -0400

    update

--- Lab3/histogram.s
+++ Lab3/histogram.s
@@ -1,32 +1,52 @@
-  PUBLIC EightBitHistogram                        ;Torna a fun??o acess?vel fora de histogram.s
+  PUBLIC EightBitHistogram                        ;Torna a funcao acessivel fora de histogram.s
 
-  SECTION .text : CODE(2)                         ;Define uma sess?o e alinhamento
-  THUMB                                           ;Instru??es do tipo thumb
+  SECTION .text : CODE(2)                         ;Define uma sessao e alinhamento
+  THUMB                                           ;Instrucoes do tipo thumb
 
 EightBitHistogram         PUSH {R4-R12, LR}              ; Salvando contexto
   MUL R0,R0,R1                   ;Numero de elementos a serem processados
   MOV R12, #65536                ;R12 guarda o valor m?ximo de 64k
-  CMP R0, R12                    ;Compara se o tamanho nao excede o tamanho m?ximo
+  CMP R0, R12                    ;Compara se o tamanho nao excede o tamanho max
+  MOV R12, #0
   IT HS
   BHS Return                     ; Caso seja maior colaca 0 no R0 e retorna para o fluxo do programa
-iter
-  LDRB R4,[R2]                ; Carrega a primera posi??o da matriz
-  STRB  R4,[R3]
-  ADD R2,R2,#1
-  ADD R3,R3,#1
-  ADD R5,R5,#1
-  CMP R5,R0
+  BL  MemInit                    ; Inicia a area de memoria a ser usada
+  MOV R4,#0
+  ADD R4, R0, R2                 ; Endereco final da iteracao na flash
+iter                             ; Iterar a matriz
+  LDRB  R1,[R2]                  ; Carrega posicao I da matriz em R1
+  ADD R3,R1                      ; Offset no endereco base que está em R3
+  ADD R3,R1                      ; Offset no endereco base que está em R3
+  BL Sum                         ; Guarda posicao I da matriz na memoria
+  SUB R3,R1                      ; Retira o offset e volta para o endereco base
+  SUB R3,R1                      ; Retira o offset e volta para o endereco base
+  ADD R2,R2,#1                   ; Percorrer os 19200 elemento na flash
+  CMP R2,R4                      ; Compara se já chegou ao final da flash
   BNE iter
+  MOV R0,R3
   POP {R4-R12, PC}               ;Voltando para o contexto
   
 
-Return
+Return                           ;Valor acima do requisito de 64k retorna para a funcao main indicando 0 como retorno
   MOV R0,#0
   POP {R4-R12, PC}               ;Voltando para o contexto
-  
-  
-  
-  
 
+
+Sum
+  LDRH R6,[R3]                   ; Carrega em R4 o valor que existe naquela posicao de memoria
+  ADD R6,R6,#1                   ; R4 = R4 + 1
+  STRH R6,[R3]
+  MOV R6,#0                      ; Zera o valor de R6
+  BX LR
+ 
+
+MemInit                         ; Funcao para dar init na memoria
+  STRH  R11,[R3]                ; Zera area de memoria
+  ADD R3, R3,#2
+  ADD R8,#1                     ;´Iterador de memoria
+  CMP R8,#512                   
+  BNE MemInit
+  MOV R3,R4                     ; R3 recebe o endereco da variavel
+  BX LR
 
   END
--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -1,14 +1,24 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include "image.h"
 
 
-extern void EightBitHistogram();
 
 int main()
 {
-
-  EightBitHistogram(4);
-  printf("Hello World");
+  uint16_t histogram;           
+  uint16_t *p_hist;         
+  p_hist = &histogram;
+  
+  uint16_t *p_hist_result;
+  
+  p_hist_result = EightBitHistogram(WIDTH1,HEIGTH1,p_start_image1, &histogram);
+  for(uint16_t i =0; i < 256     ; i = i + 1)
+  {
+    printf("posicao %#04x possui %i  pixels\n",i,p_hist[i]);
+      p_hist[i]=0;
+  }
   
   return 0;
 }
